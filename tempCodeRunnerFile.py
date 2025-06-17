cdb836af73b0267384df8ad584fa65ee9a171172
Harsh Raj <harshsinghr463@gmail.com>
2025-06-17 12:22:15 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+self.txt_gender=ttk.Combobox(self.root,textvariable=self.var_gender,values=("Select","Male","Female","Others"),font=("goudy old style",15,'bold'),state='readonly',justify=CENTER)
+        # self.txt_gender.place(x=150,y=140,width=200)
+        # self.txt_gender.current(0)
